8fd6efcc8061913616470e3a8e2c10afd98e746f
Cysoft <cuiyan@outlook.com>
2023-06-26 21:01:59 +0800

    Create init.ps1

---
+++ init.ps1
@@ -0,0 +1,24 @@
+$ver = "0.01b"
+Write-Output "Azure AMD V620(NG) Setup source for WL env"
+Write-Output $ver
+
+Write-Output "Download AMD V620 Driver"
+wget https://download.microsoft.com/download/b/5/9/b5906014-ea8f-4195-83fa-e016e11dc141/AMD-Azure-Cloud-Gaming-Driver-380774E-22Q2.exe -outfile amdv620.exe
+
+Write-Output "Download and install Azcopy"
+wget https://aka.ms/downloadazcopy-v10-windows -outfile azcopy.zip
+Expand-Archive .\azcopy.zip .\az\
+copy .\az\azcopy_windows_amd64_10.19.0\*.* C:\Windows\
+
+Write-Output "Download GS(WL)"
+azcopy cp “https://setupsource.blob.core.windows.net/file/Ver_4.16.0.3.zip?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2223-06-20T10:47:46Z&st=2023-06-20T02:47:46Z&spr=https,http&sig=wPpS0%2B9ubaL9GhqmCLHq8sA8V%2F6NoLoAxFF38vZ%2BB8s%3D”  .\Ver_4.16.0.3.zip
+Expand-Archive .\Ver_4.16.0.3.zip c:\
+
+Write-Output "Download WL Setup helper"
+azcopy cp “https://setupsource.blob.core.windows.net/file/x86BasicEnvironment.exe?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2223-06-20T10:47:46Z&st=2023-06-20T02:47:46Z&spr=https,http&sig=wPpS0%2B9ubaL9GhqmCLHq8sA8V%2F6NoLoAxFF38vZ%2BB8s%3D” .\x86BasicEnvironment.exe
+mkdir c:\Basic
+copy .\x86BasicEnvironment.exe c:\Basic
+
+Write-Output "Done"
+
+
