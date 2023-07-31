5bfdf6fabfb03101b679a3c8f255ef2a16bb5018
Cysoft <cuiyan@outlook.com>
2023-07-31 18:40:37 +0800

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -12,13 +12,13 @@ Function Install-Project()
 
 	Write-Output "Download and install Azcopy"
 	wget https://aka.ms/downloadazcopy-v10-windows -outfile azcopy.zip
-	Expand-Archive .\azcopy.zip .\az\
+	Expand-Archive .\azcopy.zip .\az\ -Force
 	copy .\az\azcopy_windows_amd64_10.19.0\*.* C:\Windows\
 
 	Write-Output "Download GS(WL)"
         $gs = "https://setupsource.blob.core.windows.net/file/Ver_4.16.0.3.zip?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2223-06-20T10:47:46Z&st=2023-06-20T02:47:46Z&spr=https,http&sig=wPpS0%2B9ubaL9GhqmCLHq8sA8V%2F6NoLoAxFF38vZ%2BB8s%3D"
 	azcopy cp $gs  .\Ver_4.16.0.3.zip
-	Expand-Archive .\Ver_4.16.0.3.zip c:\
+	Expand-Archive .\Ver_4.16.0.3.zip c:\ -Force
 
 	Write-Output "Download Well-link Basic Setup helper"
 	$helper = "https://setupsource.blob.core.windows.net/file/x86BasicEnvironment.exe?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2223-06-20T10:47:46Z&st=2023-06-20T02:47:46Z&spr=https,http&sig=wPpS0%2B9ubaL9GhqmCLHq8sA8V%2F6NoLoAxFF38vZ%2BB8s%3D"
@@ -29,7 +29,7 @@ Function Install-Project()
 	Write-Output "Download Well-link AutoVOS 1.3"
 	$autoVos = "https://setupsource.blob.core.windows.net/file/AutoVOS_v1.3.zip?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2223-06-20T10:47:46Z&st=2023-06-20T02:47:46Z&spr=https,http&sig=wPpS0%2B9ubaL9GhqmCLHq8sA8V%2F6NoLoAxFF38vZ%2BB8s%3D"
 	azcopy cp $autoVos .\AutoVOS_v1.3.zip
-	Expand-Archive  .\AutoVOS_v1.3.zip c:\AutoVOS_v1.3
+	Expand-Archive  .\AutoVOS_v1.3.zip c:\AutoVOS_v1.3 -Force
 
 	Write-Output "Done"
 }
