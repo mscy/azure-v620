b84300bbd40414773ed263b8a8ad990d02efe76d
Cysoft <cuiyan@outlook.com>
2023-08-01 20:59:17 +0800

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -13,7 +13,7 @@ Function Install-Project()
 	Write-Output "Download and install Azcopy"
 	wget https://aka.ms/downloadazcopy-v10-windows -outfile azcopy.zip
 	Expand-Archive .\azcopy.zip .\az\ -Force
-	copy .\az\azcopy_windows_amd64_10.20.0\*.* C:\Windows\
+	copy .\az\azcopy_windows_amd64*\*.* C:\Windows\
 
 	Write-Output "Download GS(WL)"
         $gs = "https://setupsource.blob.core.windows.net/file/Ver_4.16.0.3.zip?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2223-06-20T10:47:46Z&st=2023-06-20T02:47:46Z&spr=https,http&sig=wPpS0%2B9ubaL9GhqmCLHq8sA8V%2F6NoLoAxFF38vZ%2BB8s%3D"
