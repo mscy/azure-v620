8116ae45d9c9ed28f5d02ce04ee17e84e57b2c43
Cysoft <cuiyan@outlook.com>
2024-07-09 14:40:20 +0800

    Update wlperf.ps1

--- wlperf.ps1
+++ wlperf.ps1
@@ -8,9 +8,9 @@ Function Install-WlPerf()
 	Write-Output $ver
     mkdir c:\wlperf
 
-    weget https://raw.githubusercontent.com/mscy/azure-v620/main/setperf.ps1 -outfile c:\wlperf\setperf.ps1
-    weget https://raw.githubusercontent.com/mscy/azure-v620/main/CounterList.csv -outfile c:\wlperf\CounterList.csv
-    weget https://raw.githubusercontent.com/mscy/azure-v620/main/startperf.ps1 -outfile c:\wlperf\startperf.ps1
+    wget https://raw.githubusercontent.com/mscy/azure-v620/main/setperf.ps1 -outfile c:\wlperf\setperf.ps1
+    wget https://raw.githubusercontent.com/mscy/azure-v620/main/CounterList.csv -outfile c:\wlperf\CounterList.csv
+    wget https://raw.githubusercontent.com/mscy/azure-v620/main/startperf.ps1 -outfile c:\wlperf\startperf.ps1
     c:\wlperf\setperf.ps1
     Write-Output "run  c:\wlperf\startperf.ps1 to start collect perflog"
 	Write-Output "Done"
